81c3ab61896b6e47fc90476fb4d5ec572c376755
ADARSH-IIIT <adarshtech251@gmail.com>
2024-05-18 10:16:33 +0530

    jai shree ram

---
+++ _30_len_function.py
@@ -0,0 +1,46 @@
+####   used to find length of any iterable object
+
+"""
+syntax
+len(obj_name)
+
+"""
+
+
+# iterable object 
+
+"""
+string
+range
+list
+tup set dict etc
+
+
+"""
+
+
+x = range(0,10 , 2)
+
+print(len(x)) #=======> 5 
+
+
+x = "adarsh"
+print(len(x)) #=======> 6
+
+
+
+x = [1,6,True , "adarsh"]
+print(len(x)) #=======> 4
+
+
+
+
+
+x = { 
+    "fname" : "adarsh" ,
+    "lname" : "dwivedi" ,
+    "age" : 21
+
+ }
+print(len(x)) #=======> 3 
+
---
+++ _31_del_keyword.py
@@ -0,0 +1,27 @@
+"""
+
+del  keyword is used to delete any var , object completly
+
+syntax
+
+
+del name_of_obj
+
+"""
+
+
+x = 909
+
+del x 
+
+
+print(x)
+
+#error NameError: name 'x' is not defined
+
+
+
+# similaly we can delete any list tup etcc etcc 
+
+
+
